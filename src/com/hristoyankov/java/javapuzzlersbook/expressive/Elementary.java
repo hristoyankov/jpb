3ec2c3cd23e6ff5296767e985853e5ab06db6323
Hristo Yankov <ico.yankov@gmail.com>
2015-01-24 23:55:53 +0200

    Adding puzzle c01p04 - It's elementary

---
+++ src/com/hristoyankov/java/javapuzzlersbook/expressive/Elementary.java
@@ -0,0 +1,9 @@
+package com.hristoyankov.java.javapuzzlersbook.expressive;
+
+// Chapter 01 - It's elementary (Puzzle 4)
+public class Elementary {
+    public static void main(String[] args) {
+		//System.out.println(12345 + 5432l);
+		System.out.println(12345 + 54321);
+    }
+}
