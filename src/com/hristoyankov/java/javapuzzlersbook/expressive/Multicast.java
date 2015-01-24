eb800cd0e9eb125ff8370e8e084cc2cdfbbec9f4
Hristo Yankov <ico.yankov@gmail.com>
2015-01-24 23:57:19 +0200

    Adding puzzle c01p06 - Multicast

---
+++ src/com/hristoyankov/java/javapuzzlersbook/expressive/Multicast.java
@@ -0,0 +1,8 @@
+package com.hristoyankov.java.javapuzzlersbook.expressive;
+
+// Chapter 01 - Multicast (Puzzle 6)
+public class Multicast {
+    public static void main(String[] args) {
+    	System.out.println((int) (char) (byte) -1);
+    }
+}
